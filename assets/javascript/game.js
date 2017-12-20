691287cbe00014dae64f226df5e582e98cdc4ce6
Steven^C <mr.cheng.st@gmail.com^C>
2017-12-19 23:33:28 -0700

    finalized homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
 //Tekken RPG Game
+
+
 //===========================================================
 //1. Global Variables for each character with attack power, health, and counter
 //No character is selected nor defender 
@@ -18,25 +20,25 @@ var Characters = {
         name: 'Bryanfury',
         baseAttackPower: 3,
         health: 120,
-        attack: 10,
+        attack: 4,
         },
     King: {
         name: 'King',
         baseAttackPower: 2,
         health: 100,
-        attack: 11,
+        attack: 3,
         },
     Kazuyamishima: {
         name: 'Kazuyamishima',
         baseAttackPower: 8,
         health: 90,
-      	attack: 8,
+        attack: 4,
         },
    Yoshimitsu: {
         name: 'Yoshimitsu',
         baseAttackPower: 1,
         health: 180,
-        attack: 10,
+        attack: 9,
         }
     };
 
@@ -46,198 +48,192 @@ var Characters = {
 // This function will initialize the character value from the global object variables defined above
 //Creating a function to render the selected character before player chooses player
 function settingUpCharacter(chosenCharacter){
-	character.name = chosenCharacter.name;
-	character.health = chosenCharacter.health;
-	character.baseAttackPower= chosenCharacter.baseAttackPower;
-	character.attack= chosenCharacter.attack;
-	character.counterAttack= chosenCharacter.counterAttack;
+  character.name = chosenCharacter.name;
+  character.health = chosenCharacter.health;
+  character.baseAttackPower= chosenCharacter.baseAttackPower;
+  character.attack= chosenCharacter.attack;
+  character.counterAttack= chosenCharacter.counterAttack;
 }
 //Creating a function to render the selected defender before player chooses the player
 function settingUpDefenders(chosenDefenders){
-	console.log('looking at our chooice', chosenDefenders)
-	defenders.name = chosenDefenders.name;
-	defenders.health = chosenDefenders.health;
-	defenders.baseAttackPower = chosenDefenders.baseAttackPower;
-	defenders.attack = chosenDefenders.attack;
-	character.counterAttack= chosenDefenders.counterAttack;
+  console.log('looking at our choice', chosenDefenders)
+  defenders.name = chosenDefenders.name;
+  defenders.health = chosenDefenders.health;
+  defenders.baseAttackPower = chosenDefenders.baseAttackPower;
+  defenders.attack = chosenDefenders.attack;
+  character.counterAttack= chosenDefenders.counterAttack;
 }
 
 //moving other characters into enemy container grabbing their ID's/Class in html
 function movingEnemiesToTheirContainer(){
-	$('.character').removeClass('character').addClass("enemy-characters");
-	$('#enemies-container').append($(".enemy-characters"));
-	$('.enemy-characters').css("background-color", 'red');
+  $('.character').removeClass('characterzzzz').addClass("enemy-characters");
+  $('#enemies-container').append($(".enemy-characters"));
+  $('.enemy-characters').css("background-color");
 }
 
 //Now the game starts with this function
-$(document).ready(function() {
-	// $("#restart").hide();
-	
-	var audioElement = document.createElement("audio");
-	audioElement.setAttribute("src", "Assets/moonlight.mp3");
+  $(document).ready(function() {
+  // $("#restart").hide();
 
-	// Theme Button
-	$(".theme-button").on("click", function () {
-		audioElement.play();
-	});
-	    
 //================ BRYAN FURY SECTION ======================\\
 //Bryan Fury when clicked creates a function; console.log works :D
-	$('#bryanfury-character').on('click', function(){
-		console.log("Mwahahahahahahaha! Die!");
+  $('#bryanfury-character').on('click', function(){
+    console.log("Mwahahahahahahaha! Die!");
 
         //Player has not selected anything because we want to add more functionality to the character
-		if(playerSelected === false){
+    if(playerSelected === false){
 
             //When the player is established; grab the render variables to Bryan Fury
             //Now the character selected selected
-			settingUpCharacter(Characters.Bryanfury);
-			playerSelected = true;
+      settingUpCharacter(Characters.Bryanfury);
+      playerSelected = true;
 
             //display chosen player
             //Grabbing the ID character on HTML, then removing the ID and putting them into a container
-			$("#bryanfury-character").removeClass("character").addClass("chosen-characters-container");
-			$("#chosen-characters-container").append(this);
+      $("#bryanfury-character").removeClass("character").addClass("chosen-characters-container");
+      $("#chosen-characters-container").append(this);
             
             //This is established earlier and calling back this function(line  61)
-			movingEnemiesToTheirContainer()
+      movingEnemiesToTheirContainer()
 
         //If the playerSelected is selected and the defenderSelected has not than... 
-		} else if(playerSelected === true && defenderSelected === false){
-			if($("#bryanfury-character").hasClass("enemy-characters")){
-				$('#message-container').empty();
+    } else if(playerSelected === true && defenderSelected === false){
+      if($("#bryanfury-character").hasClass("enemy-characters")){
+        $('#message-container').empty();
 
                 //Setting up the defenders against Bryan Furt
-				settingUpDefenders(Characters.Bryanfury);
-				defenderSelected = true;
+        settingUpDefenders(Characters.Bryanfury);
+        defenderSelected = true;
 
                 //Selecting Bryan Furt character, removing him and adding him into the defender zone
                 $("#bryanfury-character").removeClass("enemy-characters").addClass("defender-character");
                 //Grab all this information and put that into the defender zone
-				$("#defender-container").append(this);
-            	}
+        $("#defender-container").append(this);
+              }
         }
     });
 
 //================ KING SECTION ======================\\
 //King when clicked creates a function; console.log works :D
 
-	$('#king-character').on('click', function(){
-		console.log("Hold on Marduk! After I'm through here, you're next!");
+  $('#king-character').on('click', function(){
+    console.log("Hold on Marduk! After I'm through here, you're next!");
 
-		if(playerSelected === false){
+    if(playerSelected === false){
 
-			settingUpCharacter(Characters.King);
-			playerSelected = true;
+      settingUpCharacter(Characters.King);
+      playerSelected = true;
 
-			//display chosen player
-			$('#king-character').removeClass('character').addClass('chosen-characters-container');
-			$('#chosen-characters-container').append(this);
+      //display chosen player
+      $('#king-character').removeClass('character').addClass('chosen-characters-container');
+      $('#chosen-characters-container').append(this);
 
-			movingEnemiesToTheirContainer()
-		} else if(playerSelected === true && defenderSelected === false){
-			if($('#king-character').hasClass('enemy-characters')){
+      movingEnemiesToTheirContainer()
+    } else if(playerSelected === true && defenderSelected === false){
+      if($('#king-character').hasClass('enemy-characters')){
 
-				settingUpDefenders(Characters.King);
-				defenderSelected = true;
+        settingUpDefenders(Characters.King);
+        defenderSelected = true;
 
-				$('#king-character').removeClass('enemy-characters').addClass('defender-character');
-				$('#defender-container').append(this);
-			}
-		}
-	});
+        $('#king-character').removeClass('enemy-characters').addClass('defender-character');
+        $('#defender-container').append(this);
+      }
+    }
+  });
 
 //================ KAZUYA MISHIMA SECTION ======================\\
 
-	$('#kazuya-character').on('click', function () {
-		console.log("Kisama ka.");
+  $('#kazuya-character').on('click', function () {
+    console.log("Kisama ka.");
 
-		if(playerSelected === false){
+    if(playerSelected === false){
 
-			settingUpCharacter(Characters.Kazuyamishima);
-			playerSelected= true;
+      settingUpCharacter(Characters.Kazuyamishima);
+      playerSelected= true;
 
-			$('#kazuya-character').removeClass('character').addClass('chosen-characters-container');
-			$('#chosen-characters-container').append(this);
+      $('#kazuya-character').removeClass('character').addClass('chosen-characters-container');
+      $('#chosen-characters-container').append(this);
 
-			movingEnemiesToTheirContainer()
-		}else if(playerSelected === true && defenderSelected === false){
-			if ($('#kazuya-character').hasClass('enemy-characters')) {
+      movingEnemiesToTheirContainer()
+    }else if(playerSelected === true && defenderSelected === false){
+      if ($('#kazuya-character').hasClass('enemy-characters')) {
 
-				settingUpDefenders(Characters.Kazuyamishima);
-				defenderSelected = true;
+        settingUpDefenders(Characters.Kazuyamishima);
+        defenderSelected = true;
 
-				$('#kazuya-character').removeClass('enemy-characters').addClass('defender-character');
-				$('#defender-container').append(this);
-			}
-		}
+        $('#kazuya-character').removeClass('enemy-characters').addClass('defender-container');
+        $('#defender-container').append(this);
+      }
+    }
     });
     
 //================ YOSHIMITISU SECTION ======================\\
 
-	$("#yoshimitsu-character").on('click', function(){
-		console.log("Shikisoku zekuu.");
+  $("#yoshimitsu-character").on('click', function(){
+    console.log("Shikisoku zekuu.");
 
-		if(playerSelected === false){
-			settingUpCharacter(Characters.Yoshimitsu)
-			playerSelected= true;
+    if(playerSelected === false){
+      settingUpCharacter(Characters.Yoshimitsu)
+      playerSelected= true;
 
-			$('#yoshimitsu-character').removeClass('character').addClass('chosen-characters-container');
-			$('#chosen-characters-container').append(this);
+      $('#yoshimitsu-character').removeClass('character').addClass('chosen-characters-container');
+      $('#chosen-characters-container').append(this);
 
-			movingEnemiesToTheirContainer()
-		} else if (playerSelected === true && defenderSelected === false){
-			if ($('#yoshimitsu-character').hasClass('enemy-characters')) {
+      movingEnemiesToTheirContainer()
+    } else if (playerSelected === true && defenderSelected === false){
+      if ($('#yoshimitsu-character').hasClass('enemy-characters')) {
 
-				settingUpDefenders(Characters.Yoshimitsu);
-				defenderSelected= true;
+        settingUpDefenders(Characters.Yoshimitsu);
+        defenderSelected= true;
 
-				$('#yoshimitsu-character').removeClass('enemy-characters').addClass('defender-character');
-				$('#defender-container').append(this);
-			}
-		}
-	});
+        $('#yoshimitsu-character').removeClass('enemy-characters').addClass('defender-container');
+        $('#defender-container').append(this);
+      }
+    }
+  });
 
-	$('#attack-button').on("click", function(){
-		console.log('You selected the attack button.');
+  //Attack Section=============
 
-		if(playerSelected && defenderSelected && gameOver === false){
-			// if defender are being attacked by character chosen
+  $('#attack-button').on("click", function(){
+    console.log('You selected the attack button.');
 
-			defenders.health = defenders.health - character.attack;
-			$($(`#${defenders.name} p.health`)[0]).html(defenders.health);
+    if(playerSelected && defenderSelected && gameOver === false){
+      // if defender are being attacked by character chosen
 
-
-			console.log(defenders.health);
-			console.log(playerSelected);
+      defenders.health = defenders.health - character.attack;
+      $($(`#${defenders.name} p.health`)[1]).html(defenders.health);
 
 
-			character.attack = character.attack + character.baseAttackPower;
-			$('#message-container').html('You have attacked ' + character.name + ' with ' + character.attack + ' points.');
-			console.log(character.health);
-
-			character.health = character.health - defenders.attack;
-			$($(`#${character.name} p.health`)[0]).html(character.health);
-			console.log()
+      console.log(defenders.health);
+      console.log(playerSelected);
 
 
-			if (character.health <= 0) {
-				$('#message-container').html('You have died!');
-			} else if (defenders.health <= 0) {
-				enemiesDefeated++;
-				defenderSelected = false;
+      character.attack = character.attack + character.baseAttackPower;
+      $('#message-container').html('You have attacked ' + character.name + ' with ' + character.attack + ' points.');
+      console.log(character.health);
 
-				$('#defender-container').empty();
+      character.health = character.health - defenders.attack;
+      $($(`#${character.name} p.health`)[1]).html(character.health);
+      console.log()
 
-				//  		$('#enemies-container').removeClass('enemy-characters');
-				// $('#defender-container').append(this);
 
-				if (enemiesDefeated === 3) {
-					gameOver = true;
-					$('#message-container').html('You have won!');
-					return;
-				}
-			}
-		}
-	});
+      if (character.health <= 0) {
+        $('#message-container').html('You have died!');
+      } else if (defenders.health <= 0) {
+        enemiesDefeated++;
+        defenderSelected = false;
+
+        $('#defender-container').empty();
+
+        //      $('#enemies-container').removeClass('enemy-characters');
+        // $('#defender-container').append(this);
+
+        if (enemiesDefeated === 3) {
+          gameOver = true;
+          $('#message-container').html('You have won!');
+          return;
+        }
+      }
+    }
+  });
 });
